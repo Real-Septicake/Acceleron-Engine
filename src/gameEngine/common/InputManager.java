4c964f9430d5a670d2a73842f782362d4c8e66c2
Cj <cjherman588@gmail.com>
2019-02-25 12:09:22 -0800

    Worked on adding mouse inputs to the input manager, theoretically they should work now. This needs testing so it will not be marked as complete in the projects

--- src/gameEngine/common/InputManager.java
+++ src/gameEngine/common/InputManager.java
@@ -6,6 +6,8 @@ import java.util.Dictionary;
 import java.util.HashMap;
 import java.util.Hashtable;
 
+import org.joml.Vector2d;
+
 import gameEngine.debug.Debug;
 
 public class InputManager {
@@ -13,21 +15,97 @@ public class InputManager {
 	private static HashMap<Integer, Boolean> currentKeys = new HashMap<Integer, Boolean>();
 	private static HashMap<Integer, Boolean> lastKeys = new HashMap<Integer, Boolean>();
 	
+	private static Vector2d mousePosition = new Vector2d(0, 0);
+	private static Vector2d oldMousePosition = new Vector2d(0, 0);
+	private static Vector2d mouseDelta = new Vector2d(0, 0);
+	private static HashMap<Integer, Boolean> currentMouseButtons = new HashMap<Integer, Boolean>();
+	private static HashMap<Integer, Boolean> lastMouseButtons = new HashMap<Integer, Boolean>();
+	
+	//Run an update on inputs, send old ones to the last state storage
 	public static void inputUpdate() {
 		lastKeys = new HashMap<Integer, Boolean>(currentKeys);
+		lastMouseButtons = new HashMap<Integer, Boolean>(lastMouseButtons);
+		
+		mouseDelta = new Vector2d(mousePosition.x - oldMousePosition.x, mouseDelta.y - oldMousePosition.y);
+		oldMousePosition = new Vector2d(mousePosition.x, mousePosition.y);
 	}
 	
+	//Setup the callbacks that are used for getting inputs
 	public static void setup(long windowID) {
 		glfwSetKeyCallback(windowID, (window, key, scancode, action, mods) -> {
 			updateKey(key, action);
 		});
+		
+		glfwSetCursorPosCallback(windowID, (window, xpos, ypos) -> {
+			mouseMoveHandler(window, xpos, ypos);
+		});
+		
+		glfwSetMouseButtonCallback(windowID, (window, button, action, mods) -> { 
+			mouseButtonHandler(window, button, action);
+		});
 	}
 	
-	public static void updateKey(int key, int action) {
+	//Update a keyboard input
+	private static void updateKey(int key, int action) {
 		if(action == GLFW_RELEASE || action == GLFW_PRESS)
 			currentKeys.put(key, action == GLFW_PRESS);
 	}
 	
+	//Update the mouse position
+	private static void mouseMoveHandler(long window, double xpos, double ypos) {
+		mousePosition = new Vector2d(xpos, ypos);
+	}
+	
+	//Update the mouse buttons
+	private static void mouseButtonHandler(long window, int key, int action) {
+		if(action == GLFW_RELEASE || action == GLFW_PRESS)
+			currentMouseButtons.put(key, action == GLFW_PRESS);
+	}
+	
+	//Get if a mouse button is down
+	public static boolean mouseButtonDown(int id) {
+		if(currentMouseButtons.get(id) == null)
+			return false;
+		
+		boolean b = currentMouseButtons.get(id);
+		return b;
+	}
+	
+	//Get if a mouse button is now down
+	public static boolean mouseButtonIsDown(int id) {
+		if (currentMouseButtons.get(id) == null) {
+			return false;
+		}
+		
+		if (currentMouseButtons.get(id) == true && (lastMouseButtons.get(id) == null || lastMouseButtons.get(id) == false)) {
+			return true;
+		}
+		return false;
+	}
+	
+	//Get if a mouse button is now up
+	public static boolean mouseButtonUp(int id) {
+		if (currentMouseButtons.get(id) == null) {
+			return false;
+		}
+		
+		if (currentMouseButtons.get(id) == false && (lastMouseButtons.get(id) != null && lastMouseButtons.get(id) == true)) {
+			return true;
+		}
+		return false;
+	}
+	
+	//Get the mouse position
+	public static Vector2d mousePosition() {
+		return new Vector2d(mousePosition.x, mousePosition.y);
+	}
+	
+	//Get the mouse delta
+	public static Vector2d mouseDelta() {
+		return new Vector2d(mouseDelta.x, mouseDelta.y);
+	}
+	
+	//Get if a key input is down
 	public static boolean keyDown(int id) {
 		if(currentKeys.get(id) == null)
 			return false;
@@ -36,6 +114,7 @@ public class InputManager {
 		return b;
 	}
 	
+	//Get if a key input is now down
 	public static boolean keyIsDown(int id) {
 		if (currentKeys.get(id) == null) {
 			return false;
@@ -47,6 +126,7 @@ public class InputManager {
 		return false;
 	}
 	
+	//Get if a key input is now up
 	public static boolean keyUp(int id) {
 		if (currentKeys.get(id) == null) {
 			return false;
